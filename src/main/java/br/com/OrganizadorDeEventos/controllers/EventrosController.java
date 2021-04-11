e225a9c4997c4834af4a287619fbc01f4c7236eb
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-04-10 23:20:45 -0300

    Realizado a implementação da pagina detalesEvento e a listagem de participantes do evento

--- src/main/java/br/com/OrganizadorDeEventos/controllers/EventrosController.java
+++ src/main/java/br/com/OrganizadorDeEventos/controllers/EventrosController.java
@@ -9,13 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.OrganizadorDeEventos.models.Convidado;
 import br.com.OrganizadorDeEventos.models.Evento;
+import br.com.OrganizadorDeEventos.repository.ConvidadoRepository;
 import br.com.OrganizadorDeEventos.repository.EventoRepository;
 
 @Controller
 public class EventrosController {
 	@Autowired
 	private EventoRepository er;
+	
+	@Autowired
+	private ConvidadoRepository cr;
 	
 	//Métodos para cadastro de Eventos
 		@RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
@@ -40,11 +45,23 @@ public class EventrosController {
 		}
 		
 	//Destalhes do evento
-		@RequestMapping("/{codigo}")
+		@RequestMapping(value="/{codigo}", method = RequestMethod.GET)
 		public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
 			Evento evento = er.findByCodigo(codigo);
 			ModelAndView view = new ModelAndView("evento/detalhesEvento");
 			view.addObject("evento", evento);
+			
+			Iterable<Convidado> convidados = cr.findByEvento(evento);
+			view.addObject("convidados", convidados);
 			return view;
+		}
+		
+		@RequestMapping(value="/{codigo}", method = RequestMethod.POST)
+		public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado){
+			Evento evento = er.findByCodigo(codigo);
+			convidado.setEvento(evento);
+			cr.save(convidado);
+			
+			return "redirect:/{codigo}";
 		}
 }
--- src/main/java/br/com/OrganizadorDeEventos/repository/ConvidadoRepository.java
+++ src/main/java/br/com/OrganizadorDeEventos/repository/ConvidadoRepository.java
@@ -3,7 +3,8 @@ package br.com.OrganizadorDeEventos.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.OrganizadorDeEventos.models.Convidado;
+import br.com.OrganizadorDeEventos.models.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
-	Convidado findByRg();
+	Iterable<Convidado> findByEvento(Evento evento);
 }
