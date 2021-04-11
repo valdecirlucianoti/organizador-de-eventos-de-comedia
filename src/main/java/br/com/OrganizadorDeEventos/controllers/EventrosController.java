71303c12ac92948ca48bc3a1eb024371f50089e9
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-04-11 16:04:32 -0300

    Realizado a manutenção da pagina index

--- src/main/java/br/com/OrganizadorDeEventos/controllers/EventrosController.java
+++ src/main/java/br/com/OrganizadorDeEventos/controllers/EventrosController.java
@@ -2,12 +2,17 @@ package br.com.OrganizadorDeEventos.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+import javax.validation.executable.ValidateOnExecution;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.OrganizadorDeEventos.models.Convidado;
 import br.com.OrganizadorDeEventos.models.Evento;
@@ -27,15 +32,22 @@ public class EventrosController {
 		public String form() {
 			return "evento/formEvento";
 		}
+		
 		@RequestMapping(value="/cadastrarEvento", method = RequestMethod.POST)
-		public String form(Evento evento) {
+		public String form(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
+			if(result.hasErrors()) {
+				attributes.addFlashAttribute("mensagem", "Verifique os campos");
+				return "redirect:/cadastrarEvento";
+			}
+			
 			System.out.println(evento);
 			er.save(evento);
+			attributes.addFlashAttribute("mensagem", "Evento cadastrado com sucesso");
 			return "redirect:/cadastrarEvento";
 		}
 	
 	//lista todos os eventos na index
-		@RequestMapping("/eventos")
+		@RequestMapping("/listarEventos")
 		public ModelAndView listaEventos() {
 			ModelAndView view = new ModelAndView("index");
 			
@@ -56,11 +68,17 @@ public class EventrosController {
 			return view;
 		}
 		
+	//Salvando os convidados
 		@RequestMapping(value="/{codigo}", method = RequestMethod.POST)
-		public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado){
+		public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado, BindingResult result, RedirectAttributes attributes){
+			if(result.hasErrors()) {
+				attributes.addFlashAttribute("mensagem", "Verifique os campos");
+				return "redirect:/{codigo}";
+			}
 			Evento evento = er.findByCodigo(codigo);
 			convidado.setEvento(evento);
 			cr.save(convidado);
+			attributes.addFlashAttribute("mensagem", "Convidado cadastrado com sucesso");
 			
 			return "redirect:/{codigo}";
 		}
